7c6fc37eb7715ebf614ba5844163716a01c37e29
Sergey <sergey.04.10.1991@gmail.com>
2020-09-07 14:06:23 +0300

    initial commit

---
+++ index.php
@@ -0,0 +1,5 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetPageProperty("type_of_page", "Самая главная");
+$APPLICATION->SetTitle("Интернет-магазин \"Одежда\"");
+?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
