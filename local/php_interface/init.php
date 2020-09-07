a284145a7e0aabb9710e2843f82161ef34ea1b68
Sergey <sergey.04.10.1991@gmail.com>
2020-09-07 17:34:48 +0300

    task1: fix

---
+++ local/php_interface/init.php
@@ -0,0 +1,37 @@
+<?
+  AddEventHandler("main", "OnBuildGlobalMenu", "makeForContentManager");
+
+  $eventManager = \Bitrix\Main\EventManager::getInstance();
+  $eventManager->addEventHandler("main", "OnBuildGlobalMenu", "makeForContentManager");
+
+	function makeForContentManager(&$aGlobalMenu, &$aModuleMenu) {
+		global $USER;
+
+		$arUserGroups = $USER->GetUserGroupArray();
+
+		foreach ($arUserGroups as $group) {
+
+			if ($group === "8") {
+
+				foreach ($aGlobalMenu as $key=>$value) {
+
+					if ($key !== "global_menu_content" && $key !== "global_menu_store" && $key !== "global_menu_settings") {
+						unset($aGlobalMenu[$key]);
+					}
+
+				}
+
+				foreach ($aModuleMenu as $key=>$value) {
+
+					if ($aModuleMenu[$key]["items_id"] !== "menu_iblock_/news") {
+						unset($aModuleMenu[$key]);
+					}
+
+				}
+
+			}
+
+		}
+
+	}
+?>
--- local/templates/default_eshop_template/components/bitrix/news/news_detail_custom/bitrix/news.list/.default/component_epilog.php
+++ local/templates/default_eshop_template/components/bitrix/news/news_detail_custom/bitrix/news.list/.default/component_epilog.php
@@ -1,7 +1,7 @@
-<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
 
-<?
-  print_r($arResult);
+
+  echo '<pre>'.print_r($arResult, true).'</pre>';
 
   if (isset($arResult["FIRST_NEWS_DATE"])) {
     $APPLICATION->SetPageProperty("specialdateContent", $arResult["FIRST_NEWS_DATE"]);
