6e44460d538e96ddaedd2c06295342f6ea3a8343
Sergey <sergey.04.10.1991@gmail.com>
2020-09-07 15:17:47 +0300

    task ex2_108: done

---
+++ local/templates/default_eshop_template/components/bitrix/news/news_custom/bitrix/news.detail/.default/component_epilog.php
@@ -0,0 +1,10 @@
+<? 
+	if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+
+  $canonicalElemName = $arResult["CANONICAL_ELEM_NAME"];
+  
+  if (!empty($canonicalElemName)) {
+		$APPLICATION->SetPageProperty("canonical", $canonicalElemName);
+	} 
+
+?>
