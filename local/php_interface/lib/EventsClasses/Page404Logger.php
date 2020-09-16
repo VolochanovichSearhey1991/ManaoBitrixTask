c3bb0cfbd1062f7d4032b327cb37545b0a3d8897
Sergey <sergey.04.10.1991@gmail.com>
2020-09-16 14:01:24 +0300

    task ex2-93: fixed

--- local/php_interface/lib/EventsClasses/Page404Logger.php
+++ local/php_interface/lib/EventsClasses/Page404Logger.php
@@ -8,17 +8,17 @@
             if (defined('ERROR_404') && ERROR_404 == 'Y') {
                  GLOBAL $APPLICATION;
                 $APPLICATION->RestartBuffer();
-                include $_SERVER['DOCUMENT_ROOT'].'/local/templates/default_eshop_template/header.php';
+                include $_SERVER['DOCUMENT_ROOT'] . SITE_TEMPLATE_PATH . '/header.php';
                 require ($_SERVER['DOCUMENT_ROOT'].'/404.php');
-                $page404Uri = "http://$_SERVER[HTTP_HOST]" . $APPLICATION->GetCurPage(true);
-                
+                $page404Uri = "$_SERVER[HTTP_HOST]" . $APPLICATION->GetCurPage(true);
+                echo $_SERVER["HTTP_HOST"];
                 \CEventLog::Add(array(
                     "SEVERITY" => "INFO",
                     "AUDIT_TYPE_ID" => "ERROR_404",
                     "MODULE_ID" => "main",
                     "DESCRIPTION" => $page404Uri
                 ));
-                include $_SERVER['DOCUMENT_ROOT'].'/local/templates/default_eshop_template/footer.php';
+                include $_SERVER['DOCUMENT_ROOT'] . SITE_TEMPLATE_PATH . '/footer.php';
 
                 die();
             }
