eb4662a97b37dec533d4712fb461ea65973c6dd2
Sergey <sergey.04.10.1991@gmail.com>
2020-09-17 13:58:57 +0300

    task ex2-88: done

---
+++ ex2/time-control/index.php
@@ -0,0 +1,9 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("time_control");
+?>самая ресурсоемкая страница:&nbsp;<a href="http://980trainee.dev-bitrix.by/catalog/index.php">http://980trainee.dev-bitrix.by/catalog/index.php</a><br>
+ доля: 47.47%<br>
+ <br>
+ размер кэша "по умолчанию" для компонента 46кб<br>
+ размер кэша "только с необходимыми данными" для компонента 16кб<br>
+ разница: 30кб<br><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
