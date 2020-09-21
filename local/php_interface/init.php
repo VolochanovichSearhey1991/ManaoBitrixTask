cfd2af6e3735929b08ab499a2907362f7daaa225
Sergey <sergey.04.10.1991@gmail.com>
2020-09-21 12:45:48 +0300

    task ex2-54: done

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -1,33 +1,46 @@
 <?
+
     use \Bitrix\Main\Config\Option;
     use Bitrix\Main\Mail\Event;
 
-    function CheckUserCount()
-    {
+    class NewUsersCounter {
+
+        private function getAdminUsers() {
             $filter = ['GROUPS_ID' => '1'];
             $by = ['id' => 'asc'];
             $result = CUser::GetList($by, $order = 'desc', $filter);
+            return $result;
+        }
+
+        private function dateDiffConvert($dateDiff) {
+
+            $dateFrom = new \DateTime('@0');
+            $dateTo = new \DateTime("@$dateDiff");
+            $dateDiffConvert = $dateFrom->diff($dateTo)->format('%a дней, %h часов, %i минут и %s секунд');
+            return $dateDiffConvert;
+
+        }
+
+        static function CheckUserCount()
+        {
+            $userCounter = new NewUsersCounter();
+
+            $result = $userCounter->getAdminUsers();
 
             while ($user = $result->Fetch()) {
                 $arAdminsId[] = $user['ID'];
                 $arAdminsMail[] =  $user['EMAIL'];
             }
-
+    
             $usersCountNow = CUser::GetCount();
             $dateNow = time();
             $usersCountBefore = Option::get("main", "usersCount");
             $dateBefore = Option::get("main", "date");
             $dateDiff = $dateNow - $dateBefore;
-            
-
-            $dateFrom = new \DateTime('@0');
-            $dateTo = new \DateTime("@$dateDiff");
-            $dateDiffConvert = $dateFrom->diff($dateTo)->format('%a дней, %h часов, %i минут и %s секунд');
-
             $usersCountDiff = $usersCountNow - $usersCountBefore;
-
-                Option::set("main", "usersCount", $usersCountNow);
-                Option::set("main", "date", $dateNow);
+            $dateDiffConvert = $userCounter->dateDiffConvert($dateDiff);
+            Option::set("main", "usersCount", $usersCountNow);
+            Option::set("main", "date", $dateNow);
         
             Event::send(array(
                 "EVENT_NAME" => "USER_INFO",
@@ -38,8 +51,11 @@
                     "MESSAGE" => "зарегистрировано новых пользователей: ". $usersCountDiff ." за " . $dateDiffConvert,
                 ),
             ));
-
-            return "CheckUserCount();";
+    
+            return "NewUsersCounter::CheckUserCount();";
+        }
     }
+    
+
 
 ?>
